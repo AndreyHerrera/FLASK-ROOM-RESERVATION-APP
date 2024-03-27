97f213a9502a83963ec9fd37c142abdad1045515
andreyherrera <andreyherrerac@icloud.com>
2024-03-27 18:10:12 -0500

    Update .gitignore, modify db.py to use pymongo and connect to MongoDB Atlas, and update server.py to use the new db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,19 @@
-from server import mongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from urllib.parse import quote_plus
+
+username = quote_plus('wolfangaherrerac')
+password = quote_plus('pesxiv-8wyxje-capRid')
+
+
+credentials = f"mongodb+srv://{username}:{password}@roomreservations.hhwd7hh.mongodb.net/?retryWrites=true&w=majority&appName=RoomReservations"
+client = MongoClient(credentials, server_api=ServerApi('1'))
 
 
 def create_collection_if_not_exists():
-    if 'reservations' not in mongo.db.list_collection_names():
-        mongo.db.create_collection('reservations')
+    db = client.mydatabase
+    if 'reservations' not in db.list_collection_names():
+        db.create_collection('reservations')
+        print("Colección 'reservations' creada.")
+    else:
+        print("La colección 'reservations' ya existe.")
--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify
-from flask_pymongo import PyMongo
 
-from app.database.db import create_collection_if_not_exists
+from app.database.db import client, create_collection_if_not_exists
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
-mongo = PyMongo(app)
-
 
 if __name__ == "__main__":
+    client.admin.command('ping')
     create_collection_if_not_exists()
     app.run(host='0.0.0.0', port='8083', debug=True)
